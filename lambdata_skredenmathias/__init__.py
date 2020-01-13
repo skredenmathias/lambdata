30b899cfbf0fa5d4ae284337bdf5993fa976bfc7
skredenmathias <skredenmathias@users.noreply.github.com>
2020-01-14 00:12:47 +0100

    added some proof of concept utility files

--- lambdata_skredenmathias/__init__.py
+++ lambdata_skredenmathias/__init__.py
@@ -1,8 +1,10 @@
-
+"""
+lambdata - a collection of data science helper functions
+"""
 
 import pandas as pd
 import numpy as np
 
 # sample code
 ONES = pd.DataFrame(np.ones(10))
-Zeroes = pd.DataFrame(np.zeros(50))
+ZEROS = pd.DataFrame(np.zeros(50))
---
+++ lambdata_skredenmathias/df_utils.py
@@ -0,0 +1,7 @@
+"""
+utility functions for working with DataFrames
+"""
+
+import pandas
+
+TEST_DF = pandas.DataFrame([1,2,3])
