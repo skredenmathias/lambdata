2157d6e8779d449774d1f6e314f9cc8c235727ee
skredenmathias <skredenmathias@users.noreply.github.com>
2020-01-15 00:44:27 +0100

    updated df_utils with pep8 and improved functions

--- lambdata_skredenmathias/df_utils.py
+++ lambdata_skredenmathias/df_utils.py
@@ -6,21 +6,24 @@ TEST_DF = pandas.DataFrame([1, 2, 3])
 # Set pandas max displays function
 
 
-def set_display_options(rows, columns):
+def set_display_options(rows, columns):  # 3rd: pandas option
     """
     Set Pandas display options for notebook
     """
-    rows = pd.options.display.max_rows
-    columns = pd.options.display.max_columns
-    return rows, columns  # Doesn't work (returns (60, 0))
+    pd.set_option('display.max_rows', rows)
+    pd.set_option('display.max_columns', columns)
+    return rows, columns
 
 # train / val / test split function
 
 
-def train_test_split(df):
+def train_test_split(df, random_state=42):
     """
     Create train/val/test dataframes with test_size = .2, from dataframe df
     """
-    from sklearn.model_selection import train_test_split
-    train, val = train_test_split(df, test_size=.2, random_state=42)
-    train, test = train_test_split(train, test_size=.2, random_state=42)
+    if len(df) < 3:
+        print('no bueno')
+    train, test = train_test_split(df, test_size=.2, random_state=random_state)
+    train, val = train_test_split(
+        train, test_size=.2, random_state=random_state)
+    return train, test, val
