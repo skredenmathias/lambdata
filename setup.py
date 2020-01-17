5138c7d9ba6d8d16848b6c32054192110856bf79
skredenmathias <skredenmathias@users.noreply.github.com>
2020-01-17 17:16:18 +0100

    updated several files, added unit 3 week 1 sprint md

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-skredenmathias",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "skredenmathias",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
